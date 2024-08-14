ec1917a64008b0c4d4e6d8cbdc1ce3286571e477
canoo <cano.0708@hotmail>
2024-08-13 19:12:31 -0600

    added api

--- app.js
+++ app.js
@@ -12,5 +12,8 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
+  console.log(`App and API running on port http://localhost:${PORT}`);
 });
+
+
+//api
